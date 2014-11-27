df163fd7e203ef4369840c57eacbfe1f46c58499
RobinP122 <robinplojoux@gmail.com>
2014-11-27 16:39:42 +0100

    Ajout de la form ajout contact

---
+++ WFA_Cabane/WFA_Cabane/Form_AjoutContact.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WFA_Cabane
+{
+    partial class Form_AjoutContact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edt_informations = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_ajouter = new System.Windows.Forms.Button();
+            this.btn_Annuler = new System.Windows.Forms.Button();
+            this.edt_nom = new System.Windows.Forms.TextBox();
+            this.edt_adresse = new System.Windows.Forms.TextBox();
+            this.edt_mobile = new System.Windows.Forms.TextBox();
+            this.edt_email = new System.Windows.Forms.TextBox();
+            this.edt_web = new System.Windows.Forms.TextBox();
+            this.edt_prenom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // edt_informations
+            // 
+            this.edt_informations.Location = new System.Drawing.Point(121, 191);
+            this.edt_informations.Multiline = true;
+            this.edt_informations.Name = "edt_informations";
+            this.edt_informations.Size = new System.Drawing.Size(180, 33);
+            this.edt_informations.TabIndex = 38;
+            // 
+            // label15
+            // 
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(12, 191);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(101, 33);
+            this.label15.TabIndex = 37;
+            this.label15.Text = "Informations";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(12, 165);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(101, 24);
+            this.label14.TabIndex = 36;
+            this.label14.Text = "Site Web";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(12, 139);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(101, 24);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "Email";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(12, 113);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 24);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Mobile";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(12, 88);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(101, 23);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Adresse";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(12, 38);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 24);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Nom";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 26);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Contact";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_ajouter
+            // 
+            this.btn_ajouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ajouter.Location = new System.Drawing.Point(12, 236);
+            this.btn_ajouter.Name = "btn_ajouter";
+            this.btn_ajouter.Size = new System.Drawing.Size(101, 34);
+            this.btn_ajouter.TabIndex = 44;
+            this.btn_ajouter.Text = "Ajouter";
+            this.btn_ajouter.UseVisualStyleBackColor = true;
+            this.btn_ajouter.Click += new System.EventHandler(this.btn_ajouter_Click);
+            // 
+            // btn_Annuler
+            // 
+            this.btn_Annuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Annuler.Location = new System.Drawing.Point(200, 236);
+            this.btn_Annuler.Name = "btn_Annuler";
+            this.btn_Annuler.Size = new System.Drawing.Size(101, 34);
+            this.btn_Annuler.TabIndex = 45;
+            this.btn_Annuler.Text = "Annuler";
+            this.btn_Annuler.UseVisualStyleBackColor = true;
+            // 
+            // edt_nom
+            // 
+            this.edt_nom.Location = new System.Drawing.Point(121, 38);
+            this.edt_nom.Multiline = true;
+            this.edt_nom.Name = "edt_nom";
+            this.edt_nom.Size = new System.Drawing.Size(180, 24);
+            this.edt_nom.TabIndex = 46;
+            // 
+            // edt_adresse
+            // 
+            this.edt_adresse.Location = new System.Drawing.Point(121, 88);
+            this.edt_adresse.Multiline = true;
+            this.edt_adresse.Name = "edt_adresse";
+            this.edt_adresse.Size = new System.Drawing.Size(180, 23);
+            this.edt_adresse.TabIndex = 47;
+            // 
+            // edt_mobile
+            // 
+            this.edt_mobile.Location = new System.Drawing.Point(121, 113);
+            this.edt_mobile.Multiline = true;
+            this.edt_mobile.Name = "edt_mobile";
+            this.edt_mobile.Size = new System.Drawing.Size(180, 24);
+            this.edt_mobile.TabIndex = 48;
+            // 
+            // edt_email
+            // 
+            this.edt_email.Location = new System.Drawing.Point(121, 139);
+            this.edt_email.Multiline = true;
+            this.edt_email.Name = "edt_email";
+            this.edt_email.Size = new System.Drawing.Size(180, 24);
+            this.edt_email.TabIndex = 49;
+            // 
+            // edt_web
+            // 
+            this.edt_web.Location = new System.Drawing.Point(121, 165);
+            this.edt_web.Multiline = true;
+            this.edt_web.Name = "edt_web";
+            this.edt_web.Size = new System.Drawing.Size(180, 24);
+            this.edt_web.TabIndex = 50;
+            // 
+            // edt_prenom
+            // 
+            this.edt_prenom.Location = new System.Drawing.Point(121, 63);
+            this.edt_prenom.Multiline = true;
+            this.edt_prenom.Name = "edt_prenom";
+            this.edt_prenom.Size = new System.Drawing.Size(180, 23);
+            this.edt_prenom.TabIndex = 52;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Prénom";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form_AjoutContact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 272);
+            this.Controls.Add(this.edt_prenom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edt_web);
+            this.Controls.Add(this.edt_email);
+            this.Controls.Add(this.edt_mobile);
+            this.Controls.Add(this.edt_adresse);
+            this.Controls.Add(this.edt_nom);
+            this.Controls.Add(this.btn_Annuler);
+            this.Controls.Add(this.btn_ajouter);
+            this.Controls.Add(this.edt_informations);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Name = "Form_AjoutContact";
+            this.Text = "Form_AjoutContact";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edt_informations;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_ajouter;
+        private System.Windows.Forms.Button btn_Annuler;
+        private System.Windows.Forms.TextBox edt_nom;
+        private System.Windows.Forms.TextBox edt_adresse;
+        private System.Windows.Forms.TextBox edt_mobile;
+        private System.Windows.Forms.TextBox edt_email;
+        private System.Windows.Forms.TextBox edt_web;
+        private System.Windows.Forms.TextBox edt_prenom;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WFA_Cabane/WFA_Cabane/Form_AjoutContact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+namespace WFA_Cabane
+{
+    public partial class Form_AjoutContact : Form
+    {
+        public Form_AjoutContact()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ajouter_Click(object sender, EventArgs e)
+        {
+            ConnexionDB DB = new ConnexionDB();
+
+            string requete = "INSERT INTO `personnes`(`Nom`, `Prenom`, `Adresse`, `NoMobile`, `Email`, `SiteWeb`) VALUES ([test],[wd],[wqd],[341],[adw],[qqdwawd])";
+        }
+    }
+}
