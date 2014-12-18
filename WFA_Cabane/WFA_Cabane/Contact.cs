444c875979c4c3a09434eae71383861f5c594f34
dricc1213 <ventur213@gmail.com>
2014-12-18 13:17:21 +0100

    Amelioration de la classe contact

--- WFA_Cabane/WFA_Cabane/Contact.cs
+++ WFA_Cabane/WFA_Cabane/Contact.cs
@@ -8,12 +8,53 @@ namespace WFA_Cabane
 {
     public class Contact
     {
-        public int idPersonne;
-        public string Nom;
-        public string Prenom;
-        public string Adresse;
-        public string Telephone;
-        public string Mail;
-        public string SiteWeb;
+        private int _idPersonne;
+        public int IdPersonne
+        {
+            get { return _idPersonne; }
+            set { _idPersonne = value; }
+        }
+
+        private string _Nom;
+        public string Nom
+        {
+            get { return _Nom; }
+            set { _Nom = value; }
+        }
+
+        private string _Prenom;
+        public string Prenom
+        {
+            get { return _Prenom; }
+            set { _Prenom = value; }
+        }
+
+        private string _Adresse;
+        public string Adresse
+        {
+            get { return _Adresse; }
+            set { _Adresse = value; }
+        }
+
+        private string _Telephone;
+        public string Telephone
+        {
+            get { return _Telephone; }
+            set { _Telephone = value; }
+        }
+
+        private string _Mail;
+        public string Mail
+        {
+            get { return _Mail; }
+            set { _Mail = value; }
+        }
+
+        private string _SiteWeb;
+        public string SiteWeb
+        {
+            get { return _SiteWeb; }
+            set { _SiteWeb = value; }
+        }
     }
 }
