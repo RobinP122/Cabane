2b5ed546f4e15c87293549c1e5ad8e3e38f0afe9
RobinP122 <robinplojoux@gmail.com>
2014-12-18 16:26:41 +0100

    Ajout cabane 10%

---
+++ WFA_Cabane/WFA_Cabane/Form_AjoutCabane.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WFA_Cabane
+{
+    partial class Form_AjoutCabane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edt_nbLits = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edt_menu = new System.Windows.Forms.TextBox();
+            this.edt_coordonnees = new System.Windows.Forms.TextBox();
+            this.edt_douches = new System.Windows.Forms.TextBox();
+            this.edt_altitude = new System.Windows.Forms.TextBox();
+            this.edt_nom = new System.Windows.Forms.TextBox();
+            this.btn_Annuler = new System.Windows.Forms.Button();
+            this.btn_ajouter = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edt_tarif = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // edt_nbLits
+            // 
+            this.edt_nbLits.Location = new System.Drawing.Point(111, 72);
+            this.edt_nbLits.Multiline = true;
+            this.edt_nbLits.Name = "edt_nbLits";
+            this.edt_nbLits.Size = new System.Drawing.Size(180, 23);
+            this.edt_nbLits.TabIndex = 67;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Nombre de lit";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // edt_menu
+            // 
+            this.edt_menu.Location = new System.Drawing.Point(111, 174);
+            this.edt_menu.Multiline = true;
+            this.edt_menu.Name = "edt_menu";
+            this.edt_menu.Size = new System.Drawing.Size(180, 24);
+            this.edt_menu.TabIndex = 65;
+            // 
+            // edt_coordonnees
+            // 
+            this.edt_coordonnees.Location = new System.Drawing.Point(111, 148);
+            this.edt_coordonnees.Multiline = true;
+            this.edt_coordonnees.Name = "edt_coordonnees";
+            this.edt_coordonnees.Size = new System.Drawing.Size(180, 24);
+            this.edt_coordonnees.TabIndex = 64;
+            // 
+            // edt_douches
+            // 
+            this.edt_douches.Location = new System.Drawing.Point(111, 122);
+            this.edt_douches.Multiline = true;
+            this.edt_douches.Name = "edt_douches";
+            this.edt_douches.Size = new System.Drawing.Size(180, 24);
+            this.edt_douches.TabIndex = 63;
+            // 
+            // edt_altitude
+            // 
+            this.edt_altitude.Location = new System.Drawing.Point(111, 97);
+            this.edt_altitude.Multiline = true;
+            this.edt_altitude.Name = "edt_altitude";
+            this.edt_altitude.Size = new System.Drawing.Size(180, 23);
+            this.edt_altitude.TabIndex = 62;
+            // 
+            // edt_nom
+            // 
+            this.edt_nom.Location = new System.Drawing.Point(111, 47);
+            this.edt_nom.Multiline = true;
+            this.edt_nom.Name = "edt_nom";
+            this.edt_nom.Size = new System.Drawing.Size(180, 24);
+            this.edt_nom.TabIndex = 61;
+            // 
+            // btn_Annuler
+            // 
+            this.btn_Annuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Annuler.Location = new System.Drawing.Point(187, 233);
+            this.btn_Annuler.Name = "btn_Annuler";
+            this.btn_Annuler.Size = new System.Drawing.Size(101, 34);
+            this.btn_Annuler.TabIndex = 60;
+            this.btn_Annuler.Text = "Annuler";
+            this.btn_Annuler.UseVisualStyleBackColor = true;
+            // 
+            // btn_ajouter
+            // 
+            this.btn_ajouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ajouter.Location = new System.Drawing.Point(2, 233);
+            this.btn_ajouter.Name = "btn_ajouter";
+            this.btn_ajouter.Size = new System.Drawing.Size(101, 34);
+            this.btn_ajouter.TabIndex = 59;
+            this.btn_ajouter.Text = "Ajouter";
+            this.btn_ajouter.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(2, 174);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(101, 24);
+            this.label14.TabIndex = 58;
+            this.label14.Text = "Menu";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(2, 148);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(101, 24);
+            this.label13.TabIndex = 57;
+            this.label13.Text = "Coordonnées";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(2, 122);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 24);
+            this.label12.TabIndex = 56;
+            this.label12.Text = "Douches";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(2, 97);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(101, 23);
+            this.label11.TabIndex = 55;
+            this.label11.Text = "Altitude";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(2, 47);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 24);
+            this.label10.TabIndex = 54;
+            this.label10.Text = "Nom";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(2, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(289, 26);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Cabane";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "Tarif";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // edt_tarif
+            // 
+            this.edt_tarif.Location = new System.Drawing.Point(111, 200);
+            this.edt_tarif.Multiline = true;
+            this.edt_tarif.Name = "edt_tarif";
+            this.edt_tarif.Size = new System.Drawing.Size(180, 24);
+            this.edt_tarif.TabIndex = 69;
+            // 
+            // Form_AjoutCabane
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 279);
+            this.Controls.Add(this.edt_tarif);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.edt_nbLits);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edt_menu);
+            this.Controls.Add(this.edt_coordonnees);
+            this.Controls.Add(this.edt_douches);
+            this.Controls.Add(this.edt_altitude);
+            this.Controls.Add(this.edt_nom);
+            this.Controls.Add(this.btn_Annuler);
+            this.Controls.Add(this.btn_ajouter);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Name = "Form_AjoutCabane";
+            this.Text = "Form_AjoutCabane";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edt_nbLits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edt_menu;
+        private System.Windows.Forms.TextBox edt_coordonnees;
+        private System.Windows.Forms.TextBox edt_douches;
+        private System.Windows.Forms.TextBox edt_altitude;
+        private System.Windows.Forms.TextBox edt_nom;
+        private System.Windows.Forms.Button btn_Annuler;
+        private System.Windows.Forms.Button btn_ajouter;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edt_tarif;
+    }
+}
